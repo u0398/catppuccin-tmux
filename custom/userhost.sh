55022431a6dc1e2982d5e459fd70f23df4f5c715
u0398 <ux0398@gmail.com>
2024-07-22 10:52:35 -0700

    fixed option names

--- custom/userhost.sh
+++ custom/userhost.sh
@@ -2,9 +2,9 @@ show_userhost() {
   local index icon color text module
 
   index=$1
-  icon=$(get_tmux_option "@catppuccin_host_icon" "󰒋")
-  color=$(get_tmux_option "@catppuccin_host_color" "$thm_magenta")
-  text=$(get_tmux_option "@catppuccin_host_text" "#(whoami)#[fg=$thm_magenta]@#[fg=$thm_fg]#H")
+  icon=$(get_tmux_option "@catppuccin_userhost_icon" "󰒋")
+  color=$(get_tmux_option "@catppuccin_userhost_color" "$thm_magenta")
+  text=$(get_tmux_option "@catppuccin_userhost_text" "#(whoami)#[fg=$thm_magenta]@#[fg=$thm_fg]#H")
 
   module=$(build_status_module "$index" "$icon" "$color" "$text")
 
